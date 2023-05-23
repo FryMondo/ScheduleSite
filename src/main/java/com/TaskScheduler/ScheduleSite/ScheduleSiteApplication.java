4f9e14570eaa0188b12c1a9e4c712040140087b9
FryMondo <ptachyksergei@gmail.com>
2023-05-23 16:43:02 +0300

    Initial commit

---
+++ src/main/java/com/TaskScheduler/ScheduleSite/ScheduleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.TaskScheduler.ScheduleSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleSiteApplication.class, args);
+	}
+
+}
