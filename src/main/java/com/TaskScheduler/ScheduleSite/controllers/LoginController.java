0dde73b8667715e5dfdd2046328a48c7c826d8e1
FryMondo <ptachyksergei@gmail.com>
2023-05-27 18:34:16 +0300

    Login page controller

---
+++ src/main/java/com/TaskScheduler/ScheduleSite/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.TaskScheduler.ScheduleSite.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginController(Model model) {
+        return "login";
+    }
+}
