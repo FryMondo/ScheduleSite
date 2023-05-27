a043e5e8fb2da8e362b74858f7cbc9744f4049c1
FryMondo <ptachyksergei@gmail.com>
2023-05-27 17:20:44 +0300

    Changed name of method "index" to "mainController"

--- src/main/java/com/TaskScheduler/ScheduleSite/controllers/MainController.java
+++ src/main/java/com/TaskScheduler/ScheduleSite/controllers/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String home(Model model) {
+    public String mainController(Model model) {
         model.addAttribute("title", "Головна сторінка");
         return "home";
     }
