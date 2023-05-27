40cabeaf12c2cb73e65bc42a0e3842813e7b3310
FryMondo <ptachyksergei@gmail.com>
2023-05-27 16:47:12 +0300

    Small changes

--- src/main/java/com/TaskScheduler/ScheduleSite/controllers/ErrorController.java
+++ src/main/java/com/TaskScheduler/ScheduleSite/controllers/ErrorController.java
@@ -11,16 +11,10 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
-        // Отримуємо код помилки HTTP з запиту
         int statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        // Зберігаємо повідомлення про помилку у модель
         model.addAttribute("errorMessage", "Сталася помилка. Код помилки: " + statusCode);
-
-        // Повертаємо сторінку з повідомленням про помилку
         return "error-page";
     }
-
     public String getErrorPath() {
         return "/error";
     }
