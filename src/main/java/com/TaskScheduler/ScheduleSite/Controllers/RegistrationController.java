0f4b368bc4bb2152c927effdebb8ca8376631201
FryMondo <ptachyksergei@gmail.com>
2023-06-04 14:58:14 +0300

    Added "User" for using username and password

--- src/main/java/com/TaskScheduler/ScheduleSite/Controllers/RegistrationController.java
+++ src/main/java/com/TaskScheduler/ScheduleSite/Controllers/RegistrationController.java
@@ -1,5 +1,6 @@
 package com.TaskScheduler.ScheduleSite.Controllers;
 
+import com.TaskScheduler.ScheduleSite.Users.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RegistrationController {
     @GetMapping("/registration")
     public String registrationController(Model model) {
+        model.addAttribute("user", new User());
         return "registration";
     }
 }
