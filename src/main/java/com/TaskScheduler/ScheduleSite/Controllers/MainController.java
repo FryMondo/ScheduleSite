78f4ceac0797b264c6351871054e9be59ad297cc
FryMondo <ptachyksergei@gmail.com>
2023-06-05 02:54:59 +0300

    Gives username that successfully logged in site into main page

--- src/main/java/com/TaskScheduler/ScheduleSite/Controllers/MainController.java
+++ src/main/java/com/TaskScheduler/ScheduleSite/Controllers/MainController.java
@@ -3,11 +3,13 @@ package com.TaskScheduler.ScheduleSite.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @GetMapping("/mainPage")
-    public String mainController(Model model) {
+    public String mainPage(Model model, @RequestParam(required = false) String username) {
+        model.addAttribute("username", username);
         return "task-main";
     }
 }
