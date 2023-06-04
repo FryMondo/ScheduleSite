d401ae2dd3c591e57dc5d439fd2d1bf7ac2cdc8c
FryMondo <ptachyksergei@gmail.com>
2023-06-04 16:46:20 +0300

    Added possibility to check username and password

--- src/main/java/com/TaskScheduler/ScheduleSite/Users/UserService.java
+++ src/main/java/com/TaskScheduler/ScheduleSite/Users/UserService.java
@@ -2,12 +2,22 @@ package com.TaskScheduler.ScheduleSite.Users;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public boolean checkPassword(User user, String password) {
+        return user.getPassword().equals(password);
     }
 
     public User register(User user) {
