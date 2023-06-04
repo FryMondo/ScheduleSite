ac544ddd097f1e8d5f39d40d7ab2205c6214867d
FryMondo <ptachyksergei@gmail.com>
2023-06-04 16:44:55 +0300

    Controller for login page

--- src/main/java/com/TaskScheduler/ScheduleSite/Controllers/LoginController.java
+++ src/main/java/com/TaskScheduler/ScheduleSite/Controllers/LoginController.java
@@ -1,13 +1,35 @@
 package com.TaskScheduler.ScheduleSite.Controllers;
 
+import com.TaskScheduler.ScheduleSite.Users.User;
+import com.TaskScheduler.ScheduleSite.Users.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.Optional;
 
 @Controller
 public class LoginController {
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/login")
-    public String loginController(Model model) {
+    public String login() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String signIn(@RequestParam String username, @RequestParam String password, RedirectAttributes redirectAttributes) {
+        Optional<User> user = userService.findByUsername(username);
+        if (user.isPresent() && userService.checkPassword(user.get(), password)) {
+            return "redirect:/";
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Invalid username or password");
+            return "redirect:/login";
+        }
     }
 }
